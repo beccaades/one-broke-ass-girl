cc6c6cd2645e9a1080c44713bc40adc147e1469b
beccaades <rebecca.ades@gmail.com>
2015-09-09 18:10:50 -0400

    search_by_coords method working

--- app/models/yelp_wrapper.rb
+++ app/models/yelp_wrapper.rb
@@ -5,4 +5,8 @@ class YelpWrapper
     @client = Yelp.client
   end
 
+  def search_by_coords
+    response = self.client.search_by_coordinates(coordinates = { latitude: 40.7124, longitude: -73.9670 }, params = { term: 'food', limit: 3 })
+    response.businesses[0].name
+  end
 end
